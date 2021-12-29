b4e07941f5386e3696256b1577de2da5631d695a
Mike Risse <rissem@gmail.com>
2021-12-29 12:27:10 -0900

    switch from monaco-editor-core to monaco-editor

--- example/src/client.ts
+++ example/src/client.ts
@@ -2,22 +2,26 @@
  * Copyright (c) 2018 TypeFox GmbH (http://www.typefox.io). All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-import { listen } from '@codingame/monaco-jsonrpc';
-import * as monaco from 'monaco-editor-core'
+import { listen } from "@codingame/monaco-jsonrpc";
+import * as monaco from "monaco-editor";
 import {
-    MonacoLanguageClient, MessageConnection, CloseAction, ErrorAction,
-    MonacoServices, createConnection
-} from '@codingame/monaco-languageclient';
-import normalizeUrl = require('normalize-url');
-const ReconnectingWebSocket = require('reconnecting-websocket');
+  MonacoLanguageClient,
+  MessageConnection,
+  CloseAction,
+  ErrorAction,
+  MonacoServices,
+  createConnection,
+} from "@codingame/monaco-languageclient";
+import normalizeUrl = require("normalize-url");
+const ReconnectingWebSocket = require("reconnecting-websocket");
 
 // register Monaco languages
-monaco.languages.register({
-    id: 'json',
-    extensions: ['.json', '.bowerrc', '.jshintrc', '.jscsrc', '.eslintrc', '.babelrc'],
-    aliases: ['JSON', 'json'],
-    mimetypes: ['application/json'],
-});
+// monaco.languages.register({
+//     id: 'json',
+//     extensions: ['.json', '.bowerrc', '.jshintrc', '.jscsrc', '.eslintrc', '.babelrc'],
+//     aliases: ['JSON', 'json'],
+//     mimetypes: ['application/json'],
+// });
 
 // create Monaco editor
 const value = `{
@@ -25,64 +29,74 @@ const value = `{
     "line_endings": "unix"
 }`;
 monaco.editor.create(document.getElementById("container")!, {
-    model: monaco.editor.createModel(value, 'json', monaco.Uri.parse('inmemory://model.json')),
-    glyphMargin: true,
-    lightbulb: {
-        enabled: true
-    }
+  model: monaco.editor.createModel(
+    value,
+    "json",
+    monaco.Uri.parse("inmemory://model.json")
+  ),
+  glyphMargin: true,
+  lightbulb: {
+    enabled: true,
+  },
 });
 
 // install Monaco language client services
 MonacoServices.install(monaco);
 
 // create the web socket
-const url = createUrl('/sampleServer')
+const url = createUrl("/sampleServer");
 const webSocket = createWebSocket(url);
 // listen when the web socket is opened
 listen({
-    webSocket,
-    onConnection: connection => {
-        // create and start the language client
-        const languageClient = createLanguageClient(connection);
-        const disposable = languageClient.start();
-        connection.onClose(() => disposable.dispose());
-    }
+  webSocket,
+  onConnection: (connection) => {
+    // create and start the language client
+    const languageClient = createLanguageClient(connection);
+    const disposable = languageClient.start();
+    connection.onClose(() => disposable.dispose());
+  },
 });
 
-function createLanguageClient(connection: MessageConnection): MonacoLanguageClient {
-    return new MonacoLanguageClient({
-        name: "Sample Language Client",
-        clientOptions: {
-            // use a language id as a document selector
-            documentSelector: ['json'],
-            // disable the default error handler
-            errorHandler: {
-                error: () => ErrorAction.Continue,
-                closed: () => CloseAction.DoNotRestart
-            }
-        },
-        // create a language client connection from the JSON RPC connection on demand
-        connectionProvider: {
-            get: (errorHandler, closeHandler) => {
-                return Promise.resolve(createConnection(connection, errorHandler, closeHandler))
-            }
-        }
-    });
+function createLanguageClient(
+  connection: MessageConnection
+): MonacoLanguageClient {
+  return new MonacoLanguageClient({
+    name: "Sample Language Client",
+    clientOptions: {
+      // use a language id as a document selector
+      documentSelector: ["json"],
+      // disable the default error handler
+      errorHandler: {
+        error: () => ErrorAction.Continue,
+        closed: () => CloseAction.DoNotRestart,
+      },
+    },
+    // create a language client connection from the JSON RPC connection on demand
+    connectionProvider: {
+      get: (errorHandler, closeHandler) => {
+        return Promise.resolve(
+          createConnection(connection, errorHandler, closeHandler)
+        );
+      },
+    },
+  });
 }
 
 function createUrl(path: string): string {
-    const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-    return normalizeUrl(`${protocol}://${location.host}${location.pathname}${path}`);
+  const protocol = location.protocol === "https:" ? "wss" : "ws";
+  return normalizeUrl(
+    `${protocol}://${location.host}${location.pathname}${path}`
+  );
 }
 
 function createWebSocket(url: string): WebSocket {
-    const socketOptions = {
-        maxReconnectionDelay: 10000,
-        minReconnectionDelay: 1000,
-        reconnectionDelayGrowFactor: 1.3,
-        connectionTimeout: 10000,
-        maxRetries: Infinity,
-        debug: false
-    };
-    return new ReconnectingWebSocket(url, [], socketOptions);
+  const socketOptions = {
+    maxReconnectionDelay: 10000,
+    minReconnectionDelay: 1000,
+    reconnectionDelayGrowFactor: 1.3,
+    connectionTimeout: 10000,
+    maxRetries: Infinity,
+    debug: false,
+  };
+  return new ReconnectingWebSocket(url, [], socketOptions);
 }
